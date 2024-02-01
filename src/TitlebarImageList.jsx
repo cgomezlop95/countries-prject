30e1d1abd879b3abfd42092c3e3f8c55b791ff33
cgomezlop95 <c.gomezlop95@gmail.com>
2024-02-01 21:25:28 +0100

    MUI components added for navigation bar and card index image list

---
+++ src/TitlebarImageList.jsx
@@ -0,0 +1,42 @@
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import ListSubheader from "@mui/material/ListSubheader";
+
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
+
+export function TitlebarImageList() {
+  const countryApi = "https://restcountries.com/v3.1/all";
+
+  const fetchData = async () => {
+    const response = await fetch(countryApi);
+    const jsonData = await response.json();
+    return jsonData;
+  };
+
+  const { data: countries } = useQuery({
+    queryKey: ["countries"],
+    queryFn: fetchData,
+  });
+
+  return (
+    <ImageList sx={{ width: 1000, height: 450 }} cols={4}>
+      <ImageListItem key="Subheader" >
+        <ListSubheader component="div">Countries</ListSubheader>
+      </ImageListItem>
+      {countries?.map((item, index) => (
+        <Link key={index} to={item.cca2}>
+          <ImageListItem key={index}>
+            <img
+              srcSet={`${item.flags.png}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              src={`${item.flags.png}?w=248&fit=crop&auto=format`}
+              loading="lazy"
+            />
+            <ImageListItemBar title={item.name.official} />
+          </ImageListItem>
+        </Link>
+      ))}
+    </ImageList>
+  );
+}
